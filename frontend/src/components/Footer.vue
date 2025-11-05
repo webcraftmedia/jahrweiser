24c250e391e1e6dba590d5a8d1a0c74f17cc0d89
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-11-05 01:12:16 +0100

    fix(frontend): mobile footer optimization (#8)

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,17 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <footer class="bg-white dark:bg-gray-800 absolute bottom-0 w-full border-t">
-    <div class="w-full mx-auto max-w-screen-xl p-1 md:flex md:items-center md:justify-between">
+  <footer class="bg-white dark:bg-gray-800 absolute bottom-0 w-full border-t p-1">
+    <div class="w-full mx-auto max-w-screen-xl flex items-center justify-between">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
         >{{ $t('components.Footer.copyright') }}
         <a href="https://www.webcraft-media.de/" class="hover:underline">{{
           $t('components.Footer.copyright-holder')
-        }}</a
-        >{{ $t('components.Footer.copyright-text') }}
+        }}</a>
+        <p class="inline hidden md:inline">{{ $t('components.Footer.copyright-text') }}</p>
       </span>
-      <ul
-        class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
-      >
+      <ul class="flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400">
         <li>
           <NuxtLink
             :to="{ path: 'https://www.webcraft-media.de/#!impressum' }"

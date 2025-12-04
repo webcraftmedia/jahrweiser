873bc577bee2f86c6341becfa8f52f7017d86553
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-12-04 17:39:37 +0100

    hide heading mobile

--- frontend/src/pages/admin/members/add.vue
+++ frontend/src/pages/admin/members/add.vue
@@ -6,7 +6,7 @@ definePageMeta({
 
 <template>
   <div>
-    <h1 class="text-2xl font-semibold text-gray-900 dark:text-white">
+    <h1 class="hidden md:block text-2xl font-semibold text-gray-900 dark:text-white">
       Mitglieder hinzufügen
     </h1>
   </div>

bd600967987be381d9cf1dcdd6ab6acd21c53708
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-12-04 16:02:39 +0100

    feat(other): admin-tags (#16)

---
+++ frontend/cli/admin-grant.ts
@@ -0,0 +1,27 @@
+import ICAL from 'ical.js'
+import { findUserByEmail, saveUser, X_ROLE } from '../server/helpers/dav'
+import { config } from './tools/config'
+import { check as checkEmail } from './tools/email'
+
+const email = process.argv[2]
+
+checkEmail(email)
+
+console.log(`Allow admin priviledge for ${email}.`)
+
+const users = await findUserByEmail(config, email)
+
+if (users.length !== 1) {
+  console.error('User with given email not found in dav endpoint')
+  process.exit(1)
+}
+
+const user = users[0]!
+const vcard = new ICAL.Component(ICAL.parse(user.props?.addressData))
+
+vcard.updatePropertyWithValue(X_ROLE, 'admin')
+
+const href = user.href as string
+const etag = user.props?.getetag
+
+await saveUser(config, href, etag, vcard.toString())
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,3 +1,5 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -32,11 +34,11 @@ export default defineNuxtConfig({
   runtimeConfig: {
     // The private keys which are only available within server-side
     // DAV
-    DAV_USERNAME: null,
-    DAV_PASSWORD: null,
-    DAV_URL: null,
-    DAV_URL_CAL: null,
-    DAV_URL_CARD: null,
+    DAV_USERNAME: process.env.NUXT_DAV_USERNAME,
+    DAV_PASSWORD: process.env.NUXT_DAV_PASSWORD,
+    DAV_URL: process.env.NUXT_DAV_URL,
+    DAV_URL_CAL: process.env.NUXT_DAV_URL_CAL,
+    DAV_URL_CARD: process.env.NUXT_DAV_URL_CARD,
     // SMTP
     SMTP_HOST: 'localhost',
     SMTP_PORT: (process.env.NUXT_SMTP_PORT && parseInt(process.env.NUXT_SMTP_PORT)) || 1025,

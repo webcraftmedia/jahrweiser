50e805d0cf66c3de2665a58eba7f50012c755264
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-11 20:38:07 +0100

    fix width of events

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1132,6 +1132,7 @@
     font-size: 0.9em;
     line-height: 1.05;
     transition: box-shadow 0.2s ease;
+    min-width: calc(100% - 1px);
     margin-left: 0.5px;
     padding: 4px 6px !important;
     border-inline-start-width: 3px !important;
